672f2f834dc57eb26c4a47b1165ee13d2fed0cb2
ANawle <149271449+ANawle@users.noreply.github.com>
2024-09-25 17:27:17 +0530

    Update python_dependencies.sh

--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo chown -R ubuntu:ubuntu ~//home/ubuntu/Aviawscodedeploy-master
+sudo chown -R ubuntu:ubuntu ~/home/ubuntu/Aviawscodedeploy-master
 virtualenv /home/ubuntu/Aviawscodedeploy-master/venv
 source /home/ubuntu/Aviawscodedeploy-master/venv/bin/activate
 pip install -r /home/ubuntu/Aviawscodedeploy-master/requirements.txt
